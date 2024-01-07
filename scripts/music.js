369fc53a83cc1b3cb33893f76680d0db05cf3b44
tsumihq <142854650+tsumihq@users.noreply.github.com>
2024-01-07 13:13:19 -0600

    Update music.js

--- scripts/music.js
+++ scripts/music.js
@@ -20,8 +20,8 @@ const songs = {
         url: '/media/music/other/knidlmainmenu.mp3'
     },
     s2: {
-        title: 'Kirbys Dream Land 3 - Cast Roll',
-        url: '/media/audio/dl3_cast.mp3'
+        title: 'Pokemon FireRed & LeafGreen - Pallet Town',
+        url: '/media/music.other/pkmfrlgpallettown.mp3'
     },
     s3: {
         title: 'Kirbys Dream Land 3 - Grassland 2',
@@ -31,42 +31,6 @@ const songs = {
         title: 'Kirbys Dream Land 3 - Grassland 4',
         url: '/media/audio/dl3_grassland_4.mp3'
     },
-    s5: {
-        title: 'Kurukuru Kururin - Cakeland',
-        url: '/media/audio/kurukuru_cakeland.mp3'
-    },
-    s6: {
-        title: 'MegaMan ZX - Green Grass Gradation',
-        url: '/media/audio/megamanzx_greengrass.mp3'
-    },
-    s7: {
-        title: 'MegaMan 8 - Astroman',
-        url: '/media/audio/megaman8_astroman.mp3'
-    },
-    s8: {
-        title: 'MegaMan 8 - Aquaman',
-        url: '/media/audio/megaman8_aquaman.mp3'
-    },
-    s9: {
-        title: 'Sonic The Hedgehog 3 - Endless Mine',
-        url: '/media/audio/sonic3_endlessmine.mp3'
-    },
-    s10: {
-        title: 'Pokemon Mystery Dungeon: Explorers of Sky - Craggy Coast',
-        url: '/media/audio/craggy_coast.mp3'
-    },
-    s11: {
-        title: 'Mario &amp; Luigi: Bowsers Inside Story - Grasslands, All The Way',
-        url: '/media/audio/grasslands_alltheway_bowser.mp3'
-    },
-    s12: {
-        title: 'Wario Ware Inc. Mega Microgames - Drifting Away',
-        url: '/media/audio/driftingaway.mp3'
-    },
-    s13: {
-        title: 'Kirby Super Star Ultra - Cushy Cloud',
-        url: '/media/audio/superstar_cushycloud.mp3'
-    }
 };
 
 function getRandom(arr) {
